ac86e4fa431184f889c6a81010b3257454bcaaae
Ben Mordue <benmordue@gmail.com>
2013-07-30 22:35:14 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RReadIt::Application.config.secret_key_base = '35a28ebac37eb52270415694ab4619ef26cc2088b3d4690e68509cade0afdec5bc1b14aaabb9274de3defdb33c02d8fef08a689229cfdb96f78d6f76cd256119'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RReadIt::Application.config.session_store :cookie_store, key: '_RReadIt_session'
